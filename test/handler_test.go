7f52cf704758658d0b89b1bb634205bb1c908b58
Vivek Avasare <avasarevivek08@gmail.com>
2025-01-26 12:57:16 +0530

    Integrate test for financial handler

---
+++ test/handler_test.go
@@ -0,0 +1,23 @@
+// handler_test.go
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	api "api-request-handler/internal/api"
+	services "api-request-handler/internal/services"
+)
+
+func TestHandler(t *testing.T) {
+	handler := api.NewHandler()
+
+	req := httptest.NewRequest("GET", "/api/financials?companyId=test", nil)
+	w := httptest.NewRecorder()
+
+	handler.FinancialsHandler(services.DummyFinancialData)(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status OK, got %d", w.Code)
+	}
+}
